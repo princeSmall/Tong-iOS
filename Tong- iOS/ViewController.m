301ea61ff5ea3be2666779068992d0f1c2a62437
le.tong <le.tong@ecovacs.com>
2021-06-01 10:00:32 +0800

    project commit

--- Tong- iOS/ViewController.m
+++ Tong- iOS/ViewController.m
@@ -6,8 +6,15 @@
 //
 
 #import "ViewController.h"
+#import "TongStudent.h"
+#import "TongStudent+TongExtension.h"
+#import "TongStudent+TongHigh.h"
+#import "TongProtocol.h"
 
-@interface ViewController ()
+
+@interface ViewController ()<TongProtocol>
+
+@property (nonatomic, strong)TongStudent *student;
 
 @end
 
@@ -15,7 +22,58 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    NSLog(@"%@",[self getStudentName]);
+    
+    NSLog(@"%@",[self getStudentAddress]);
+    
+    NSLog(@"%ld", [self.student getStudentSeatNumber:@"Tong"]);
+    
     // Do any additional setup after loading the view.
+}
+
+// category
+- (NSString *)getStudentName{
+    self.student.name = @"Tong";
+    [self.student gotoHightSchoolEveryData];
+    return self.student.name;
+}
+
+/** extension
+ 声明私有属性
+ 声明私有方法
+ 声明私有成员变量
+ */
+
+- (NSString *)getStudentAddress{
+    self.student.address = @"suzhou";
+    [self.student gotoSchoolEveryDay];
+    return self.student.address;
+}
+
+- (TongStudent *)student{
+    if (!_student) {
+        _student = [[TongStudent alloc]init];
+        _student.delegate = self;
+    }
+    return _student;
+}
+
+/**protocol
+ 
+ */
+
+//optional
+- (NSInteger)getStudentSeatNumber:(NSString *)name{
+    if ([name isEqualToString:@"Tong"]) {
+        return 110;
+    }else{
+        return 1;
+    }
+}
+
+//require
+- (NSInteger)getStudentCode{
+    return 100;
 }
 
 
---
+++ Tong- iOS/category/TongStudent.h
@@ -0,0 +1,22 @@
+//
+//  TongStudent.h
+//  Tong- iOS
+//
+//  Created by Tongle on 2021/5/31.
+//
+
+#import <Foundation/Foundation.h>
+#import "TongProtocol.h"
+
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TongStudent : NSObject
+
+@property (nonatomic, assign) id <TongProtocol> delegate;
+
+- (NSInteger)getStudentSeatNumber:(NSString *)name;
+
+@end
+
+NS_ASSUME_NONNULL_END
